03f683fbb5c867d125daaa33b8b530073c88c9ce
Arne Seemann <arne.seemann@palasthotel.de>
2026-03-20 21:26:17 +0100

    Add first STI map

---
+++ Game/maps/tiles/desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="wueste" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8" tilerendersize="grid">
+ <image source="desert.png" width="265" height="199"/>
+</tileset>
