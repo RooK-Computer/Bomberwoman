51bce32b83f7d10f5d8c7ac350ed25ada1167175
Enno Welbers <enno@valiant.local>
2026-03-21 16:31:19 +0100

    now 4 players, being called by the first four inputs

--- Game/gameinput.lua
+++ Game/gameinput.lua
@@ -1,9 +1,28 @@
 GameInput = InputHandler:extend()
 
 function GameInput:new()
+    self.gamepad = nil
+    self.player = nil
+end
+
+function GameInput:AllowedInputDevices()
+    if self.gamepad == nil then 
+        return GameInput.super.AllowedInputDevices(self)
+    else
+        return {self.gamepad}
+    end
 end
 
 function GameInput:OnRelease(joystick, button)
+    if self.gamepad == nil then
+        local newInput = GameInput()
+        newInput.gamepad = joystick:getID()
+        newInput.player = UnoccupiedPlayers:pop()
+        inputManager.HandlerStack:push(newInput)
+        newInput:OnRelease(joystick, button)
+        return
+    end
+    local currentPlayer = self.player
     if button == "dpright" then
         currentPlayer:moveRight()
     end
