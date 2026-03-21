c9be0164af86a40f926d19831487cd15a329533e
Arne Seemann <arne.seemann@palasthotel.de>
2026-03-21 14:01:36 +0100

    Add push lib and make game fullscreen

--- Game/main.lua
+++ Game/main.lua
@@ -6,22 +6,33 @@ sprites = love.graphics.newImage('assets/sprites.png')
 sprites:setFilter("nearest", "nearest", 1)
 sprites:setWrap("clampzero", "clampzero")
 
+local push = require "lib/push"
+local windowWidth, windowHeight = love.window.getDesktopDimensions()
+push:setupScreen(1920, 1080, windowWidth, windowHeight, {fullscreen = true})
+
 require "model"
 
 LoadedLevel = sti("maps/bomberplane.lua",{},0,184)
+LoadedLevel:resize(1920,1080)
 
 CurrentLevel = Level(LoadedLevel)
 
 CurrentLevel:print()
 
+
 CurrentLevel:spawnItem(Bomb(3,3))
+CurrentLevel:spawnItem(Player(15,1))
 
 function love.update(dt)
     CurrentLevel:update(dt)
 end
 
 function love.draw()
+    push:start()
+
     CurrentLevel:draw()
+
+    push:finish()
 end
 
 function love.keypressed(keycode)
--- Game/model/player.lua
+++ Game/model/player.lua
@@ -5,7 +5,23 @@ function Player:new(x,y)
     self.moves = true
     self.destructible = true
     self.occupies = true
+
+    local grid = anim8.newGrid(9,14,sprites:getWidth(),sprites:getHeight(),0,33)
+    self.animation_idle = anim8.newAnimation(grid(1,'1-2'),0.6)
 end
+
+function Player:update(dt)
+    Player.super.update(self, dt)
+    self.animation_idle:update(dt)
+end
+
+function Player:draw()
+    --   self.anim:draw(self.spritesheet, self.x, self.y, nil, game.scale)
+    love.graphics.push()
+    love.graphics.scale(8,8)
+    self.animation_idle:draw(sprites,2,2)
+    love.graphics.pop()
+end    
 
 function Player:moveRight()
     self.level:moveItem(self,1,0)
